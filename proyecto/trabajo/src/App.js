5f9009fff661055859c8dd8075ff1188c89c4097
CrisPerezNavarro <cpereznavarro7@gmail.com>
2023-06-20 21:08:02 -0600

    Add

--- proyecto/trabajo/src/App.js
+++ proyecto/trabajo/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Navbar from './Componentes/navbarSW';
 import Carrusel from './Componentes/carrusel';
+import Footer from './Componentes/Footer';
 function App() {
   return (
     <div className="App">
@@ -9,7 +10,7 @@ function App() {
         <Navbar></Navbar>
       </header>
       <Carrusel></Carrusel>
-      <p>Hola</p>
+      <Footer></Footer>
     </div>
 
   );
