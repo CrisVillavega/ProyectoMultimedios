b7a8ba7b9059b082a1df203d03d88ab34f09bf1d
CrisVillavega <cristofer.venegasvillalobos@ucr.ac.cr>
2023-06-20 20:38:15 -0600

    Actual App

--- proyecto/trabajo/src/App.js
+++ proyecto/trabajo/src/App.js
@@ -4,13 +4,12 @@ import Navbar from './Componentes/navbarSW';
 import Carrusel from './Componentes/carrusel';
 function App() {
   return (
-
-   
     <div className="App">
       <header>
         <Navbar></Navbar>
       </header>
       <Carrusel></Carrusel>
+      <p>Hola</p>
     </div>
 
   );
